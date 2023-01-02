2024a5e45b985315e7522e81d570a784ae365c03
bbastanza <b.bastanza@gmail.com>
2023-01-02 14:30:34 -0500

    send message to to me:)

--- overnight-weather.js
+++ overnight-weather.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 const axios = require('axios')
 
-const owKey = process.env.OPENWEATHER_API_KEY
+const openWeatherApiKey = process.env.OPENWEATHER_API_KEY
 const twilioSid = process.env.TWILIO_ACCOUNT_SID
 const twilioToken = process.env.TWILIO_AUTH_TOKEN
+const weatherBotSid = process.env.WEATHER_BOT_SID
+const client = require('twilio')(twilioSid, twilioToken);
 
-function main() {
-
-}
-
-main()
+const ALERT_TEMP_MIN = 32
 
 // CHARLOTTE NC
 const location = {
@@ -18,22 +16,63 @@ const location = {
 }
 
 async function getWeather() {
-  const weather = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${location.lat}&lon=${location.long}&appid=${owKey}`);
-
-  console.log(weather.data)
-  // return
+  const weather = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${location.lat}&lon=${location.long}&appid=${openWeatherApiKey}`);
 
   const list = weather.data.list;
+
+  return findColdestTemp(list)
+}
+
+function findColdestTemp(list) {
+  let lowestTemp = null
+  let timestamp = null
 
   for (let i = 0; i < list.length; i++) {
     const listItem = list[i]
 
-    console.log(listItem.dt_txt)
+    if (!lowestTemp) {
+      lowestTemp = listItem.main.temp
+      timestamp = listItem.dt_txt
+      continue
+    }
 
-    console.log(listItem.main.temp)
-
-    console.log("")
-
+    if (listItem.main.temp < lowestTemp) {
+      lowestTemp = listItem.main.temp
+      timestamp = listItem.dt_txt
+      continue
+    }
   }
+
+  return {
+    temp: lowestTemp,
+    timestamp
+  }
+
 }
 
+async function sendMessage(body) {
+  const message = await client.messages
+    .create({
+      body,
+      messagingServiceSid: weatherBotSid,
+      to: '+15304487215'
+    })
+
+  return message
+}
+
+async function main() {
+  const result = await getWeather()
+
+  // alert if our temp is lower than our alert and exit from script
+  if (result.temp < ALERT_TEMP_MIN) {
+    const message = await sendMessage(`It will be a low of ${result.temp} at ${result.timestamp}. Have a great evening!`)
+    console.log("message sent at " + result.timestamp)
+    console.log(message)
+    return
+  }
+
+  console.log("Temp did not hit minimum. Lowest temp is " + result.temp)
+}
+
+main()
